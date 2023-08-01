3322b944ab5c679395d8b048d58999d2b05a8384
A.D. Téllez <127765518+DamianTellez@users.noreply.github.com>
2023-08-01 15:09:30 +0200

    Update La_Pasteleria_de_Mari.py

--- La_Pasteleria_de_Mari.py
+++ La_Pasteleria_de_Mari.py
@@ -1,10 +1,21 @@
+#función para mostrar los sabores 
+def listaSabores():
+
+    for sabores in lista_pasteles:
+        print("\t-", sabores)
+    else:
+        print('Si necesitas un sabor en especifico dimelo')
+
+    
 lista_pasteles = ['Choco', 'Nalanja', 'Quezito', 'Choco Blanco']
 
 print('Bienvenido/a a la Pasteleria de Mari')
 name = input('¿Cuál es tu nombre?')
 print('Encantada de conocerte', name, 'Aqui podemos ofrecerte gran variedad de pastelines')
-print('¿De que sabor quieres tu pastel? Tenemos los siguientes sabores')
-for sabores in lista_pasteles:
-    print(sabores)
-else:
-    print('Si necesitas un sabor en especifico dimelo')
+print('¿De que sabor quieres tu pastel? Tenemos los siguientes sabores:')
+
+listaSabores() #llamada de la función 
+
+saborEscogido = input()
+
+print('Muy bien! Pastelín de',saborEscogido,'saliendo!')
