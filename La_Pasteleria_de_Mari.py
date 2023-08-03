fdde980239d85ec4b8ac3f793eea10ceee40a3b4
MariDev <134427300+LittleMari@users.noreply.github.com>
2023-08-03 12:02:59 +0200

    Update La_Pasteleria_de_Mari.py
    Pastelería de Mari mejorada :)

--- La_Pasteleria_de_Mari.py
+++ La_Pasteleria_de_Mari.py
@@ -1,4 +1,3 @@
-#función para mostrar los sabores 
 def listaSabores():
 
     for sabores in lista_pasteles:
@@ -6,16 +5,27 @@ def listaSabores():
     else:
         print('Si necesitas un sabor en especifico dimelo')
 
-    
-lista_pasteles = ['Choco', 'Nalanja', 'Quezito', 'Choco Blanco']
-
+#Primer cliente    
+lista_pasteles = ['Choco', 'Naranja', 'Quesito', 'Choco Blanco']
 print('Bienvenido/a a la Pasteleria de Mari')
-name = input('¿Cuál es tu nombre?')
+name = input('¿Cuál es tu nombre? ')
 print('Encantada de conocerte', name, 'Aqui podemos ofrecerte gran variedad de pastelines')
 print('¿De que sabor quieres tu pastel? Tenemos los siguientes sabores:')
-
-listaSabores() #llamada de la función 
-
+listaSabores() 
 saborEscogido = input()
-
 print('Muy bien! Pastelín de',saborEscogido,'saliendo!')
+nuevo_cliente = input('¿Hay algún cliente más? ')
+
+#El resto de clientes
+while nuevo_cliente.lower() == 'si':
+    print('Bienvenido/a a la Pasteleria de Mari')
+    name = input('¿Cuál es tu nombre? ')
+    print('Encantada de conocerte', name, 'Aqui podemos ofrecerte gran variedad de pastelines')
+    print('¿De que sabor quieres tu pastel? Tenemos los siguientes sabores:')
+    listaSabores() 
+    saborEscogido = input()
+    print('Muy bien! Pastelín de',saborEscogido,'saliendo!')
+    nuevo_cliente = input('¿Hay algún cliente más? ')
+
+#Cuando no hay más clientes
+print('Entonces me tomo un descanso 🥰')
